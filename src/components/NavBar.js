6170d4cd42580b94953e5d43485828d266df5122
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-08-11 20:51:01 +0500

    Updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -34,7 +34,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <div className="maroof">{"< Maroof />"}</div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -60,7 +60,7 @@ export const NavBar = () => {
                 Skills
               </Nav.Link>
               <Nav.Link
-                href="#projects"
+                href="#project"
                 className={
                   activeLink === "projects"
                     ? "active navbar-link"
@@ -73,14 +73,23 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#">
-                  <img src={navIcon1} alt="" />
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/developermaroof/"
+                >
+                  <img src={navIcon1} alt="linkedinIcon" />
                 </a>
-                <a href="#">
-                  <img src={navIcon2} alt="" />
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/developermaroofpage/"
+                >
+                  <img src={navIcon2} alt="facebookIcon" />
                 </a>
-                <a href="#">
-                  <img src={navIcon3} alt="" />
+                <a
+                  target="_blank"
+                  href="https://www.instagram.com/developer_maroof/"
+                >
+                  <img src={navIcon3} alt="instagramIcon" />
                 </a>
               </div>
               <HashLink to="#connect">
